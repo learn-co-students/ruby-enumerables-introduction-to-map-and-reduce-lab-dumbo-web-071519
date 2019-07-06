26ae9f736e4a722f8e083a3f13ff142a4cc3b53a
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-06 19:25:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # My Code here....
+def map_to_negativize(source_array)
+    answer = source_array.map { |n| n > 0 ? -n : -n }
+  return answer
+end 
+
+def map_to_no_change(source_array)
+  source_array.map { |n| n }
+end
+
+
+def map_to_double(source_array)
+  answer  = source_array.map { |n| n * 2 }
+  return answer
+end
+
+# def map_to_square(source_array)
+
+
+
+
+def reduce_to_total(source_array)
+ while source
+end 
+
+
+
+
+  
+  
+  
+  
