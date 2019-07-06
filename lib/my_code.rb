80fc961d2be754fab26727a32d4c04f660acc03c
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-06 19:43:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,19 +14,36 @@ def map_to_double(source_array)
   return answer
 end
 
-# def map_to_square(source_array)
+def map_to_square(source_array)
+ answer  = source_array.map { |n| n * n }
+  return answer
+end
 
+def reduce_to_total(source_array, starting_point = 0)
+new = starting_point
+i = 0
+while i < source_array.length do
+  new = new + source_array[i]
+  i += 1
+end
+return new 
+end
 
+def reduce_to_all_true(source_array)
+i = 0 
+while i < source_array.length do
+return false if !source_array[i]
+end 
+i += 1 
+return true
+end
 
-
-def reduce_to_total(source_array)
- while source
-endd 
-
-
-
-
-  
-  
-  
+  def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do 
+    return true if source_array[i]
+    i += 1 
+  end 
+  return false 
+end
   
